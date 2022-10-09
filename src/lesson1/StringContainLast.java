4398966a43764223ec858d73a4ec1c5996935ee4
ranamohamed8790 <rana.mohamed8790@gmail.com>
2022-10-09 20:05:24 +0200

    Add String Contain Task

--- src/lesson1/StringContainLast.java
+++ src/lesson1/StringContainLast.java
@@ -2,23 +2,23 @@ package lesson1;
 
 import java.util.Scanner;
 
-public class StringEqualTask {
+public class StringContainLast {
     public static void main(String[] args)
     {
         // Details of 1st User
-        String firstUser = new String();
+        String str1 = new String();
         System.out.println("Please Enter your Name");
         Scanner sc1 = new Scanner(System.in);
-        firstUser = sc1.next();
+        str1 = sc1.next();
 
         // Details of 1st User
-        String secondUser = new String();
+        String str2 = new String();
         System.out.println("Please Enter your Name");
         Scanner sc2 = new Scanner(System.in);
-        secondUser = sc1.next();
+        str2 = sc1.next();
         sc2.close();
 
         // Print the Result
-        System.out.println(firstUser.equals(secondUser));
+        System.out.println(str1.contains(str2));
     }
 }
