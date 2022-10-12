2150509040fd2aa232835252c6a791df1653df2f
ranamohamed8790 <rana.mohamed8790@gmail.com>
2022-10-12 19:09:05 +0200

    Add Welcome User Task using String object and For Loop

--- src/lesson1/WelcomeUserTask.java
+++ src/lesson1/WelcomeUserTask.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class WelcomeUserTask {
     public static void main(String[] args) {
-        String name;
+        String [] name = new String[10];
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please Enter Your Name");
-        name = sc.nextLine();
 
         for (int i =0; i<=10; i++) {
+            System.out.println("Please Enter Your Name");
+            name[0] = sc.nextLine();
+            System.out.println("Welcome " + name[0]);
+
+
 
 
         }
