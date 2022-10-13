efa08c3a33e1dacbc0607c8450a75b8a00041587
rana.mohamed8790 <rana.mohamed8790@gmail.com>
2022-10-14 00:48:45 +0200

    add task of utill package in lesson 5

--- src/OOP/CurrentTime.java
+++ src/OOP/CurrentTime.java
@@ -1,8 +1,16 @@
 package OOP;
 
+import java.util.Date;
+
 public class CurrentTime {
     public static void main(String[] args){
         long time = System.currentTimeMillis();
-        System.out.println(time);
+        System.out.println("The Time is: " + time + "\n");
+
+        Date date = new Date();
+        //System.out.println("Current time = " + date);
+        System.out.println("Current Date = " + date.getTime());
+        System.out.println("Number = " + Math.random());
+
     }
 }
