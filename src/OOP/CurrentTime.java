1c8052fdb912068e794d85caf5af2064ced409df
rana.mohamed8790 <rana.mohamed8790@gmail.com>
2022-10-14 14:52:15 +0200

    update in the class

--- src/OOP/CurrentTime.java
+++ src/OOP/CurrentTime.java
@@ -6,8 +6,8 @@ public class CurrentTime {
     public static void main(String[] args){
         long time = System.currentTimeMillis();
         System.out.println("The Time is: " + time + "\n");
-
         Date date = new Date();
+
         //System.out.println("Current time = " + date);
         System.out.println("Current Date = " + date.getTime());
         System.out.println("Number = " + Math.random());
