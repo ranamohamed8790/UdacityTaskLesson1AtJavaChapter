12f2b11ebe0b275d45bfffd6b6ab818e953e95d7
rana.mohamed8790 <rana.mohamed8790@gmail.com>
2022-10-14 14:07:13 +0200

    Create Random Class and add Example for it

---
+++ src/OOP/RandomClass.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class RandomClass {
+    public static void main(String[] args){
+        System.out.println("Print Random Number from Class of Math.Random");
+        for (int i=0; i<20; i++){
+            System.out.println(Math.random());
+        }
+    }
+}
